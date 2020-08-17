88c54e9daa8bd439eff5e4bcb209e1dad42aeda6
Sam Junaid <samj@ensemble.com>
2020-08-17 19:12:03 +0100

    updated array 1 with a selection

--- src/com/RahulShetty/Arrays/Array1.java
+++ src/com/RahulShetty/Arrays/Array1.java
@@ -6,7 +6,7 @@ public class Array1 {
 
     public static void main(String[] args) {
 
-       int a [] = new int[5];
+       int a [] = new int[4];
         a[0] = 10;
         a[1] = 7;
         a[2] = 72;
@@ -14,9 +14,9 @@ public class Array1 {
 
         int b [] = {1,34,62,85,9};
 
-        for (int i=0; i<b.length; i++)
+        for (int i=0; i<a.length; i++)
 
-        System.out.println(b[i]);
+        System.out.println(a[i]);
 
 
 
