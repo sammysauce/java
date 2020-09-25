91a1145b724e2f23223579ea9173a6b68aed6eb5
Sam Junaid <samj@ensemble.com>
2020-09-25 17:28:11 +0100

    Constructor classes added

--- src/com/RahulShetty/Arrays/Array1.java
+++ src/com/RahulShetty/Arrays/Array1.java
@@ -6,17 +6,27 @@ public class Array1 {
 
     public static void main(String[] args) {
 
-       int a [] = new int[4];
+        //Interger data type
+        int a[] = new int[4];
         a[0] = 10;
         a[1] = 7;
         a[2] = 72;
         a[3] = 2;
 
-        int b [] = {1,34,62,85,9};
+        int b[] = {1, 34, 62, 85, 9};
 
-        for (int i=0; i<a.length; i++)
+        for (int i = 0; i < a.length; i++)
 
-        System.out.println(a[i]);
+            System.out.println(a[i]);
+
+    //String datatype
+    String s[] = new String[3];
+    s[0] = "Hello";
+    s[1] = "Good monring";
+    s[2] = "Bye";
+
+    System.out.println(s[1]);
+
 
 
 
--- src/com/RahulShetty/Arrays/Array2.java
+++ src/com/RahulShetty/Arrays/Array2.java
@@ -12,16 +12,16 @@ public class Array2 {
         a[1][1] = 6;
         a[1][2] = 579;
 
-       // System.out.println(a[1][2]);
+        System.out.println(a[1][2]);
 
       /*  int b[][] = {{2, 4, 5}, {3,6,2},{2,4,8}};
-        System.out.println([2][1]);*/
+        System.out.println([2][1]);
 
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j <3; j++)
             {
                 System.out.println(a[i][j]);
             }
-        }
+        }*/
     }
 }
